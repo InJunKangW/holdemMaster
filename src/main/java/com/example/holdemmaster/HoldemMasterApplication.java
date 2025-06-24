a169b69ea969b0205f79f10b9b55ba12fedfb440
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-06-24 16:49:07 +0900

    init

---
+++ src/main/java/com/example/holdemmaster/HoldemMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.holdemmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldemMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldemMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holdemmaster/HoldemMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holdemmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldemMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
