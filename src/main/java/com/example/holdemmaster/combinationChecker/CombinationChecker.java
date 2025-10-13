47ea229e3cde283a620e0bb79bc3cad1a3091b49
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-10-14 02:15:27 +0900

    족보 계산기가, 핸드와 보드를 별도의 파라미터로 받도록 변경

--- src/main/java/com/example/holdemmaster/combinationChecker/CombinationChecker.java
+++ src/main/java/com/example/holdemmaster/combinationChecker/CombinationChecker.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import com.example.holdemmaster.domain.Card;
 import com.example.holdemmaster.combination.HoldemCombination;
+import com.example.holdemmaster.domain.HoldemHand;
 
 public interface CombinationChecker {
-	boolean matches(List<Card> cards);
+	boolean matches(HoldemHand holdemHand, List<Card> Boards);
 	HoldemCombination getCombination();
 }
